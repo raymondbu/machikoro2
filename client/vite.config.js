17c5cb5246701ce861d618cceddeabfd0cd3d873
Raymond Bu <raymond060895@gmail.com>
2026-06-29 01:03:29 +0800

    Refactor code structure for improved readability and maintainability

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'icons/*.png'],
+      includeAssets: ['favicon.svg', 'icons/*.png'],
       manifest: {
         name: 'Machi Koro 2',
         short_name: 'MachiKoro',
@@ -20,17 +20,17 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icons/machikoro_icon_purple_192.png',
+            src: '/icons/machikoro_custom_192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icons/machikoro_icon_purple_512.png',
+            src: '/icons/machikoro_custom_512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/icons/machikoro_icon_purple_512.png',
+            src: '/icons/machikoro_custom_512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
